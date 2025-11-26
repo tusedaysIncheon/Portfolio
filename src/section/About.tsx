5d6c8f090fbc516928970a9459e891d5387c7bce
Jeonjaeman <wjswoaks741@gmail.com>
2025-11-26 23:38:13 +0900

    포트폴리오

--- src/section/About.tsx
+++ src/section/About.tsx
@@ -1,16 +1,22 @@
+import Me from '@/assets/me.png'
+
 export default function About() {
     return (
         <section id="about" className="pt-20 pb-20 w-full h-screen flex-col scroll-mt-14 text-center">
-            <div className="max-w-7xl mx-auto px-6 border-2">
-                <div >
-                    <div className="items-center justify-center text-5xl font-bold">About Me.</div>
-                    <div className="flex">
-                        <div className="border-2 w-100 h-100 pointer-events-none justify-between m-15 rounded-full">img</div>
-                        <div className="border-2 w-170 h-120 mt-7 ml-10 p-15">
-                            <div id="description" className="text-left flex-col">
-                                <p className="font-bold text-5xl">전재만 <span className="font-bold text-2xl">Jaeman Jeon</span></p>
-                                <p className="mt-2 ml-1 flex">Full-stack Developer | Nickname: <p className="font-bold ml-1">Zeon</p></p>
-                                <div className="text-xl mt-8">
+            <div className="max-w-7xl mx-auto px-6 mt-40">
+                <div>
+                    <h2 className="text-4xl md:text-5xl font-bold  md:mb-30 mb-20">About Me.</h2>
+                    <div className="flex flex-col md:flex-row items-center justify-center gap-8 md:gap-35">
+                        {/* Image */}
+                        <div className="w-80 h-80 md:w-85 md:h-85 shrink-0">
+                            <img src={Me} alt="Jaeman Jeon" className="rounded-full shadow-lg" />
+                        </div>
+                        {/* Description */}
+                        <div className="text-center md:text-left mb-13">
+                            <div id="description">
+                                <p className="font-bold text-3xl md:text-5xl">전재만 <span className="font-bold text-lg md:text-2xl">Jaeman Jeon</span></p>
+                                <p className="mt-2 text-base">Full-stack Developer | Nickname: <span className="font-bold">Zeon</span></p>
+                                <div className="text-sm md:text-lg mt-4 md:mt-6 text-gray-600 dark:text-gray-300">
                                     <p className="mt-1">테크니컬 세일즈로 현장에서 사람과 기술을 연결해오며,</p>
                                     <p className="mt-1"> 문제를 이해하고 해결하는 법을 배워왔습니다.</p>
                                     <p className="mt-1">이제는 그 경험을 바탕으로, 사용자의 불편을 해결하는 </p>
